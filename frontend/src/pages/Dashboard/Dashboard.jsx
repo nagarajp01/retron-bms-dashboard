8193fbc934d9e3e9d94a14b7208b92a0d2655fbf
nagarajp01 <nagarajpatlolla1@gmail.com>
2026-07-22 15:29:16 +0530

    Dashboard test

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,35 @@
-import Navbar from "../../components/layout/Navbar/Navbar";
-import Footer from "../../components/layout/Footer/Footer";
 import PageContainer from "../../components/layout/PageContainer/PageContainer";
-
-import DashboardContent from "../../components/dashboard/DashboardContent/DashboardContent";
 
 export default function Dashboard() {
   return (
-    <>
-      <PageContainer>
-
-        <Navbar />
-
-        <DashboardContent />
-
-        <Footer />
-
-      </PageContainer>
-    </>
+    <PageContainer>
+      <h1 style={{ color: "white", fontSize: "40px" }}>
+        Dashboard Test
+      </h1>
+    </PageContainer>
   );
 }
+
+
+
+// import Navbar from "../../components/layout/Navbar/Navbar";
+// import Footer from "../../components/layout/Footer/Footer";
+// import PageContainer from "../../components/layout/PageContainer/PageContainer";
+
+// import DashboardContent from "../../components/dashboard/DashboardContent/DashboardContent";
+
+// export default function Dashboard() {
+//   return (
+//     <>
+//       <PageContainer>
+
+//         <Navbar />
+
+//         <DashboardContent />
+
+//         <Footer />
+
+//       </PageContainer>
+//     </>
+//   );
+// }
