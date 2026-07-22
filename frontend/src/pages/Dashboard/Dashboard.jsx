1609b9c365720c5b12a4e07feb231880bb922e6c
nagarajp01 <nagarajpatlolla1@gmail.com>
2026-07-22 15:48:26 +0530

    Restore original dashboard

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,35 +1,36 @@
+import Navbar from "../../components/layout/Navbar/Navbar";
+import Footer from "../../components/layout/Footer/Footer";
 import PageContainer from "../../components/layout/PageContainer/PageContainer";
+
+import DashboardContent from "../../components/dashboard/DashboardContent/DashboardContent";
 
 export default function Dashboard() {
   return (
-    <PageContainer>
-      <h1 style={{ color: "white", fontSize: "40px" }}>
-        Dashboard Test
-      </h1>
-    </PageContainer>
+    <>
+      <PageContainer>
+
+        <Navbar />
+
+        <DashboardContent />
+
+        <Footer />
+
+      </PageContainer>
+    </>
   );
 }
 
 
 
-// import Navbar from "../../components/layout/Navbar/Navbar";
-// import Footer from "../../components/layout/Footer/Footer";
-// import PageContainer from "../../components/layout/PageContainer/PageContainer";
 
-// import DashboardContent from "../../components/dashboard/DashboardContent/DashboardContent";
+// import PageContainer from "../../components/layout/PageContainer/PageContainer";
 
 // export default function Dashboard() {
 //   return (
-//     <>
-//       <PageContainer>
-
-//         <Navbar />
-
-//         <DashboardContent />
-
-//         <Footer />
-
-//       </PageContainer>
-//     </>
+//     <PageContainer>
+//       <h1 style={{ color: "white", fontSize: "40px" }}>
+//         Dashboard Test
+//       </h1>
+//     </PageContainer>
 //   );
 // }
